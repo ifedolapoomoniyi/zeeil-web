d182755bc22e9e90ed7bdd83a2a2e6f184f1a30f
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-11-22 15:27:00 +0100

    data for cards

---
+++ src/lib/data.js
@@ -0,0 +1,42 @@
+export const benefitsData = [
+	{
+		text: "Fast, efficient, and always on time. We ensure your shipments move at the speed of your business.",
+		color: "white",
+		header: "Swift",
+		icon: "/icons/icon2.png",
+	},
+	{
+		text: "Count on us for consistent, dependable deliveries that keep your business moving forward.",
+		color: "secondary",
+		header: "Reliable",
+		icon: "/icons/security-lock.png",
+	},
+	{
+		text: "From pick-up to delivery, your shipments are safeguarded with the highest standards of security and care.",
+		color: "white",
+		header: "Secured",
+		icon: "/icons/icon1.png",
+	},
+];
+
+export const testimonials = [
+	{
+		text: `They handle our logistics with care
+and precision. We've never had
+smoother operations—swift, secure, 
+and stress-free!`,
+		name: "Olorundare Osebiekwin",
+		title: "Supply Chain Manager FeyiWears",
+		bg: "primary",
+		color: "secondary",
+		avatar: "/face1.jpeg",
+	},
+	{
+		text: `"We've never had issues with Zeeil, on time deliveries with great customer support and feedback"`,
+		name: "Ismail Omayoza",
+		title: "Shoe Designer",
+		bg: "secondary",
+		color: "primary",
+		avatar: "/face2.jpeg",
+	},
+];
